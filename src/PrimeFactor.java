e9b057eebb41a691f11a16b1675937e07b882418
SleekPanther <macrobyte2@gmail.com>
2016-05-23 12:05:47 -0400

    2d arraylist kind of works

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TreeSet;
 
+
+
 public class PrimeFactor {
 	public static void main(String[] args) {
 		PrimeFactor pFact = new PrimeFactor();
@@ -9,7 +11,7 @@ public class PrimeFactor {
 		System.out.println( ( pFact.getPrimeFactors(90) ) );
 	}
 	
-	public ArrayList<Integer> getPrimeFactors(int num){
+	public ArrayList<ArrayList<Integer>> getPrimeFactors(int num){
 		// (hashset, return array, len=0>prime, 1 & num NOT prime,
 		//actually 2d arraylist, do a contains, if(contains){arr.get(index).add(currentVal++), else{add, with 2nd col=1
 		
@@ -21,10 +23,17 @@ public class PrimeFactor {
 //		System.out.println( factors );
 		
 		//new stuff
-		ArrayList<Integer> factors = new ArrayList<Integer>();
+		//private ArrayList<ArrayList<TetrisSquare>> boardSquares = new ArrayList<ArrayList<TetrisSquare>>();
+		ArrayList<ArrayList<Integer>> factors = new ArrayList<ArrayList<Integer>>();
+		
+		int numFactors = 0;
 		for(int divisor = 2; num>1; divisor++){
 			for(; num%divisor ==0; num /=divisor){
-				factors.add(divisor);		//add the prime divisor to list
+				factors.add(new ArrayList<Integer>());
+				//System.out.println("num Fact "+numFactors);
+				factors.get(numFactors++).add(divisor);		//add the prime divisor to list, & increment how many factors exist
+				
+				//System.out.println("num Fact "+numFactors);
 			}
 		}
 		return factors;
