1165f8b6d38354d5439077b3661a05fb160e0329
SleekPanther <macrobyte2@gmail.com>
2016-05-23 12:41:52 -0400

    back to 1d arraylistr

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.TreeSet;
-
-
 
 public class PrimeFactor {
 	public static void main(String[] args) {
@@ -11,29 +8,16 @@ public class PrimeFactor {
 		System.out.println( ( pFact.getPrimeFactors(90) ) );
 	}
 	
-	public ArrayList<ArrayList<Integer>> getPrimeFactors(int num){
+	public ArrayList<Integer> getPrimeFactors(int num){
 		// (hashset, return array, len=0>prime, 1 & num NOT prime,
 		//actually 2d arraylist, do a contains, if(contains){arr.get(index).add(currentVal++), else{add, with 2nd col=1
 		
-//		TreeSet<Integer> factors = new TreeSet<Integer>();
-//		factors.add(3);
-//		factors.add(4);
-//		factors.add(1);
-//		factors.add(3);
-//		System.out.println( factors );
 		
 		//new stuff
-		//private ArrayList<ArrayList<TetrisSquare>> boardSquares = new ArrayList<ArrayList<TetrisSquare>>();
-		ArrayList<ArrayList<Integer>> factors = new ArrayList<ArrayList<Integer>>();
-		
-		int numFactors = 0;
+		ArrayList<Integer> factors = new ArrayList<Integer>();
 		for(int divisor = 2; num>1; divisor++){
 			for(; num%divisor ==0; num /=divisor){
-				factors.add(new ArrayList<Integer>());
-				//System.out.println("num Fact "+numFactors);
-				factors.get(numFactors++).add(divisor);		//add the prime divisor to list, & increment how many factors exist
-				
-				//System.out.println("num Fact "+numFactors);
+				factors.add(divisor);		//add the prime divisor to list
 			}
 		}
 		return factors;
